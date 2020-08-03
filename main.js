e6a33daa33d98d85552dea8733ae7cdc66fc44b4
NicholasSharp <47288929+NicholasLKSharp@users.noreply.github.com>
2020-08-04 01:03:36 +0200

    Works

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const http = require('http');
+var streamBuffers = require('stream-buffers');
+const Stream = require('stream')
 const { prefix, token } = require("./config.json");
 const ytdl = require("ytdl-core");
 const { Console } = require("console");
@@ -78,7 +80,9 @@ client.on("voiceStateUpdate", async (oldMember, newMember)=>{
   let oldUserChannel = oldMember.channel;
 	let newUserChannel = newMember.channel;
   checkData(oldMember.guild);
+  const guildID = oldMember.guild.id;
   var prefab = guildData.get(oldMember.guild.id);
+  
   var userid = newMember.member.id;
   var type = 0;
   if(newUserChannel===null)//user left
@@ -99,13 +103,15 @@ client.on("voiceStateUpdate", async (oldMember, newMember)=>{
 });
 
 client.on("presenceUpdate", async message => {
+
   if(message===undefined)
   return;
-  if(!guildData.get(message.guild.id))
+  const guildID = message.guild.id;
+  var prefab;
+  if(!(prefab = guildData.get(guildID)))
   return;
-  prefab = guildData.get(message.guild.id);
   var userid = message.userID;
-  if(prefab.userID != userid)
+  if(prefab.userID != userid)//user matches
   return;
   
   var activities = message.user.presence.activities;
@@ -123,13 +129,12 @@ client.on("presenceUpdate", async message => {
   }
   if(!found)
   return "Spotify is not the current status";
-
+  //spotify is an activity
   var songname = activity.details;
   var artist = activity.state;
-  if(prefab.song!==null && songname==prefab.song.title)
-    return;
+  
   console.log("Presence updated");
-  guildData.set(prefab.guildID, prefab);
+  
   var d = Date.now();
   http.get("http://youtube-scrape.herokuapp.com/api/search?q=" + prefab.query.replace("$title", songname).replace("$artist", artist) +"&page=1", function(res) {
     let data = '',
@@ -139,7 +144,7 @@ client.on("presenceUpdate", async message => {
       data += stream;
     });
     res.on('end', function() {
-      prefab = guildData.get(message.guild.id);
+      var prefab = guildData.get(guildID);
       json_data = JSON.parse(data);
       var video = json_data.results[0].video;
       var duration = video.duration;
@@ -148,14 +153,13 @@ client.on("presenceUpdate", async message => {
         title: songname,
         artist: artist,
         url: video.url,
-        length: sec*1000
+        data: null,
+        delay: 0
       };
-      console.log(sec);
-      prefab.song = song;
-      prefab.delay = Date.now()-d;
-      guildData.set(prefab.guildID, prefab);
-      console.log("waiting " + prefab.delay + " ms");
-      setTimeout(play, 0, prefab)
+      song.delay = Date.now() - d;
+      guildData.set(guildID, prefab);
+      process(guildID, song);
+      
     });
   });
   return "0";
@@ -175,7 +179,7 @@ function checkData(guild){
       textChannel: null,
       voiceChannel: null,
       connection: null,
-      song: null,
+      songs: [],
       volume: 5,
       userID: null,
       playing: false,
@@ -186,47 +190,69 @@ function checkData(guild){
   }
 }
 
-async function play(prefab) {
-  var song = prefab.song;
-  if (!song) {
-    prefab.voiceChannel.leave();
-    return;
-  }
+async function playStream(guildID){
+  prefab = guildData.get(guildID);
+  var song = prefab.songs.shift();
+  console.log('begin');
+  console.log(song.data.length);
+  var d = Date.now();
+  var myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer({
+    frequency: 10,      // in milliseconds.
+    chunkSize: 2048     // in bytes.
+  }); 
   
-  var start = Date.now();
-  var first = true;
-
+  // With a buffer
+  myReadableStreamBuffer.put(song.data);
+  
   let dispatcher = prefab.connection
-    .play(ytdl(song.url, {
-      filter: 'audioonly',
-      format: 'lowestvideo',
-    }))
-    .on("speaking", ()=>{
-      
-      if(first){
-        first =false;
-      prefab = guildData.get(prefab.guildID);
-      console.log("first run");
-      prefab.delay += Date.now()-start;
-      start = Date.now();
-      prefab.playing = true;
-      console.log("delay=" +prefab.delay);
-      prefab.song.length-=prefab.delay;
-      guildData.set(prefab.guildID, prefab);
-      }
-    })
-    .on("finish", () => {
-      prefab = guildData.get(prefab.guildID);
-      console.log("natural stop");
-      prefab.playing = false;
-      guildData.set(prefab.guildID, prefab);
-    })
-    .on("error",  ()=>{
-    });
-   
-  dispatcher.setVolumeLogarithmic();
-  dispatcher.setVolumeLogarithmic(prefab.volume / 5);
-  prefab.textChannel.send(`Start playing: **${song.title}**`);
+  .play(myReadableStreamBuffer)
+  .once("speaking", ()=>{
+    prefab = guildData.get(prefab.guildID);
+    console.log("speaking");
+    prefab.textChannel.send(`Start playing: **${song.title}**`);
+    song.delay += Date.now()-d;
+    console.log("delay=" +song.delay);
+    prefab.delay = song.delay+500;
+    prefab.playing = true;
+    guildData.set(prefab.guildID, prefab);
+  })
+  .on("finish", () => {
+    prefab = guildData.get(prefab.guildID);
+    console.log("natural stop");
+    prefab.playing = false;
+    guildData.set(prefab.guildID, prefab);
+  })
+  .on("error",  (e)=>{
+    console.log(e);
+  });
+ 
+dispatcher.setVolumeLogarithmic();
+dispatcher.setVolumeLogarithmic(prefab.volume / 5);
+}
+
+async function process(guildID, song) {
+  console.log("initializing stream");
+  var start = Date.now();
+  
+  var bufs = [];
+  
+  ytdl(song.url, { filter : 'audioonly' }).on("data", (chunk)=>{
+    bufs.push(chunk);
+  }).on("end",()=>{
+    var buf = Buffer.concat(bufs);
+    var prefab = guildData.get(guildID);
+    song.delay += Date.now()-start;
+    console.log("song saved");
+    song.data = buf;
+    prefab.songs.push(song);
+    if(prefab.playing)
+      prefab.delay = 0;
+    setTimeout(playStream, prefab.delay, guildID);
+
+    guildData.set(guildID, prefab);
+  });
+  
+  
 }
 
 client.login(token);
