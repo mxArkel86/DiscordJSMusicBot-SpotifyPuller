8a26abe68c87a741cf937fe0aeb27b3979281b14
NicholasKading <47288929+NicholasLKSharp@users.noreply.github.com>
2021-05-10 09:43:49 -0700

    Added more error codes

--- errorcodes.js
+++ errorcodes.js
@@ -5,15 +5,21 @@ module.exports = {
     return flag + "A configuration is missing for the discord server " + x + '. If this is your first time running the server, please consider adding the server to the config file as mentioned in ./README.md';
   },
   errorcode_2: function (x) {
-    return flag + "A user by the query term " + x + " could not be found in your channel";
+    return flag + "A user by the query string \"" + x + "\" could not be found in your channel";
   },
   errorcode_3: function (x) {
-    return flag + "The user by the username of " + x + " is not connected to a voice channel";
+    return flag + "The user by the name of \"" + x + "\" is not connected to a voice channel";
   },
   errorcode_4: function (x) {
     return flag + "You are not connected to a voice channel " + x;
   },
   errorcode_5: function (x) {
     return flag + "You are not connected to the same voice channel as the bot " + x;
+  },
+  errorcode_6: function (x) {
+    return flag + "Multiple users were found with the specified query string \"" + x + "\"";
+  },
+  errorcode_7: function () {
+    return flag + "You are not connected to the same channel as the specified user";
   }
 }
